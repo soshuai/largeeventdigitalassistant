9abfb7b6a43dde8be98b73b7928f7fa6625b4801
wangshuai <601647787@qq.com>
2026-09-18 17:55:01 +0800

    问题修改

--- app/src/main/java/com/largeevent/management/ui/CarVerifyFragment.java
+++ app/src/main/java/com/largeevent/management/ui/CarVerifyFragment.java
@@ -74,7 +74,7 @@ public class CarVerifyFragment extends BaseFragment implements NfcCallback {
     private BroadcastReceiver funKeyReceiver;
     private long lastHandleTriggerElapsedMs = 0L;
 
-    private final boolean test = funKeyReceiver.getAbortBroadcast();
+    private final boolean test = false;
 
     @Nullable
     @Override
